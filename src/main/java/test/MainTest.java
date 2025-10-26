67ebb539792952237a884bbb1d57f6d063e9d09f
diary8 <dairy.fitahiana@gmail.com>
2025-10-26 20:48:01 +0300

    main

--- src/main/java/test/MainTest.java
+++ src/main/java/test/MainTest.java
@@ -60,7 +60,7 @@ public class MainTest {
             }
         }
     }
-    }
+    
 
    public static void main(String[] args) throws Exception {
     // Recherche toutes les classes avec l'annotation @Controller
